9638b66c7b3211f2237093b949f98ca9cb881653
piglet166 <35857842+piglet166@users.noreply.github.com>
2020-04-18 21:52:22 -0400

    GameStarted

--- Explorer/Assets/Scripts/Player/PlayerInput.cs
+++ Explorer/Assets/Scripts/Player/PlayerInput.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerInput : MonoBehaviour
 {
+    
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,32 @@ public class PlayerInput : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.A)) Walk(true);
+        if (Input.GetKey(KeyCode.D)) Walk(false);
+        if (Input.GetKey(KeyCode.Q)) Dash(true);
+        if (Input.GetKey(KeyCode.E)) Dash(false);
+        if (Input.GetKeyDown(KeyCode.Space)) Jump();
+        if (Input.GetMouseButtonDown(1)) Melee();
+        if (Input.GetMouseButtonDown(0)) Shoot();
+    }
+
+    void Walk(bool Left) {
+
+    }
+
+    void Dash(bool Left) {
+
+    }
+
+    void Jump() {
+
+    }
+
+    void Melee() {
+
+    }
+
+    void Shoot() {
+
     }
 }
--- Explorer/Assets/Scripts/Player/PlayerMovement.cs
+++ Explorer/Assets/Scripts/Player/PlayerMovement.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    Rigidbody2D rb;
+    Animator anim;
+
+    public float maxSpeed;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
